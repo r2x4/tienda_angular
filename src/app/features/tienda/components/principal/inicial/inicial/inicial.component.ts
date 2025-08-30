ee38d87a8ffae26f809011e57686eaed8ae10aeb
Rodrigo <rrs230780@gmail.com>
2025-08-30 18:24:02 -0500

    imagenesprincipal

--- src/app/features/tienda/components/principal/inicial/inicial/inicial.component.ts
+++ src/app/features/tienda/components/principal/inicial/inicial/inicial.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { INICIAL_IMAGES } from '../../../../constants/image-data';
 
 @Component({
   selector: 'app-inicial',
@@ -8,12 +9,5 @@ import { CommonModule } from '@angular/common';
   templateUrl: './inicial.component.html',
 })
 export default class InicialComponent {
-  images = [
-    { src: 'assets/calzado_TennisAdidas1.jpg', alt: 'Tennis Adidas', title: 'Tennis Adidas' },
-    { src: 'assets/implementos_Balon_Futbol.jpg', alt: 'Balón de Fútbol', title: 'Balón de Fútbol' },
-    { src: 'assets/ropa_Sudadera1.jpg', alt: 'Sudadera Deportiva', title: 'Sudadera' },
-    { src: 'assets/suplementos_Proteina_Whey.jpg', alt: 'Proteína Whey', title: 'Proteína Whey' },
-    { src: 'assets/calzado_tennis1.jpg', alt: 'Tennis Blanco', title: 'Tennis Blanco' },
-    { src: 'assets/implementos_Saco_Boxeo.jpg', alt: 'Saco de Boxeo', title: 'Saco de Boxeo' },
-  ];
+  images = INICIAL_IMAGES;
 }
